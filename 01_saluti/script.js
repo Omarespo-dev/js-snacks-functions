9778df4b7660c5529512a08f8c412a1bb3238ca3
Omarespo-dev <omaresposito21@gmail.com>
2025-01-09 13:29:39 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+// console.log(userName)
+
+// Dichiara la funzione qui.
+function accettaStringa(nome){
+    console.log("ciao", userName)
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+accettaStringa(userName)
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
